50b141d3de0d02d1dcb5428387c386227c8c61eb
asyncedd <isynqquwu@proton.me>
2023-07-12 20:30:59 +0900

    feat: Make the description better :D

--- src/components/description.svelte
+++ src/components/description.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+	let currentIndex = 0;
+	let texts = ['', 'Avid ricer', '', 'The average Linux enjoyer'];
 	let i = 0;
 	const speed = 50; /* The speed/duration of the effect in milliseconds */
 	let text = '';
@@ -9,7 +11,8 @@
 	function typeWriter() {
 		const element = document.getElementById('demo');
 		const cursor = document.getElementById('cursor');
-		const txt = element.dataset.value;
+		const txt = texts[currentIndex];
+
 		if (i < txt.length) {
 			text += txt.charAt(i);
 			element.innerHTML = text;
@@ -21,8 +24,38 @@
 			cursor.style.visibility = 'visible';
 		}
 
+		if (i === txt.length && currentIndex < texts.length - 1) {
+			currentIndex++;
+			if (txt === '') {
+				backspaceEffect();
+				return;
+			}
+			i = 0;
+			text = '';
+			setTimeout(typeWriter, 1000); // Delay between texts
+			return;
+		}
+
 		if (i < txt.length + 1) {
 			setTimeout(typeWriter, speed);
+		}
+	}
+
+	function backspaceEffect() {
+		const element = document.getElementById('demo');
+		const cursor = document.getElementById('cursor');
+		const txt = texts[currentIndex];
+		const currentText = element.innerHTML;
+
+		if (currentText.length > 0) {
+			element.innerHTML = currentText.slice(0, -1);
+			setTimeout(backspaceEffect, speed);
+		} else {
+			showCursor = false;
+			cursor.style.visibility = 'hidden';
+			i = 0;
+			text = '';
+			setTimeout(typeWriter, 1000); // Delay between texts
 		}
 	}
 
@@ -33,7 +66,7 @@
 
 <div class="text-ctp-base m-8 text-[25px] w-[15rem] text-center">
 	<div class="text-container">
-		<p id="demo" data-value="Ricer" />
+		<p id="demo" />
 		<span id="cursor" class="cursor" />
 	</div>
 </div>
@@ -42,6 +75,7 @@
 	.text-container {
 		position: relative;
 		display: inline-block;
+    white-space: nowrap;
 	}
 
 	.cursor {
