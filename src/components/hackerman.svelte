15726f92faa6f8b8da26bee26cc34094448c58db
asyncedd <isynqquwu@proton.me>
2023-07-16 06:08:31 +0900

    refactor(hackerman): Refactor using modern Typescript features

--- src/components/hackerman.svelte
+++ src/components/hackerman.svelte
@@ -4,10 +4,10 @@
 	function hackerMan(): void {
 		const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()';
 
-		const h1Elements = document.querySelectorAll('h1');
+		const h1Elements = Array.from(document.querySelectorAll('h1'));
 
 		h1Elements.forEach((h1) => {
-			h1.onmouseover = (event) => {
+			h1.addEventListener('mouseover', (event) => {
 				if (event.target.classList.contains('animating')) {
 					return; // Exit if already animating
 				}
@@ -15,36 +15,35 @@
 				event.target.classList.add('animating'); // Add class to indicate animation
 
 				let iteration = 0;
-				let interval;
+				let interval: ReturnType<typeof setInterval> | undefined;
 
-				clearInterval(interval);
+				if (interval) {
+					clearInterval(interval);
+				}
 
 				interval = setInterval(() => {
-					event.target.innerText = event.target.innerText
-						.split('')
+					event.target.innerText = Array.from(event.target.innerText)
 						.map((letter, index) => {
 							if (iteration === 0) {
 								return letters[Math.floor(Math.random() * letters.length)];
 							} else if (index < iteration) {
-								return event.target.dataset.value[index];
-							} else if (index === iteration) {
-								if (letter === event.target.dataset.value[index]) {
-									return letter; // Stop generating for this letter
-								}
+								return event.target.dataset.value![index];
+							} else if (index === iteration && letter === event.target.dataset.value![index]) {
+								return letter; // Stop generating for this letter
 							}
 
 							return letters[Math.floor(Math.random() * letters.length)];
 						})
 						.join('');
 
-					if (iteration >= event.target.dataset.value.length) {
-						clearInterval(interval);
+					if (iteration >= event.target.dataset.value!.length) {
+						clearInterval(interval!);
 						event.target.classList.remove('animating'); // Remove class after animation
-					} else if (event.target.innerText[iteration] === event.target.dataset.value[iteration]) {
+					} else if (event.target.innerText[iteration] === event.target.dataset.value![iteration]) {
 						iteration++; // Move to the next letter
 					}
 				}, 30);
-			};
+			});
 		});
 	}
 
