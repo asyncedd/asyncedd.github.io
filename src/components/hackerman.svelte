3ab3d786e7f7f82261c94d6316848ef38580c35a
asyncedd <isynqquwu@proton.me>
2023-07-16 06:30:27 +0900

    refactor(hackerman): Extract the randomizer function

--- src/components/hackerman.svelte
+++ src/components/hackerman.svelte
@@ -9,46 +9,58 @@
 
 		for (const h1 of h1Elements) {
 			const targetValue = h1.dataset.value;
-			h1.addEventListener('mouseover', (event) => {
-				const target = event.target as HTMLHeadingElement; // Type assertion to HTMLHeadingElement
 
-				if (target.classList.contains('animating')) {
-					return; // Exit if already animating
-				}
-
-				target.classList.add('animating'); // Add class to indicate animation
-
-				let iteration = 0;
-				let interval: ReturnType<typeof setInterval> | undefined;
-
-				if (interval) {
-					clearInterval(interval);
-				}
-
-				interval = setInterval(() => {
-					target.innerText = Array.from(target.innerText)
-						.map((letter, index) => {
-							if (iteration === 0) {
-								return letters[Math.floor(Math.random() * letters.length)];
-							} else if (index < iteration) {
-								return targetValue![index];
-							} else if (index === iteration && letter === targetValue![index]) {
-								return letter; // Stop generating for this letter
-							}
-
-							return letters[Math.floor(Math.random() * letters.length)];
-						})
-						.join('');
-
-					if (iteration >= target.dataset.value!.length) {
-						clearInterval(interval!);
-						target.classList.remove('animating'); // Remove class after animation
-					} else if (target.innerText[iteration] === targetValue![iteration]) {
-						iteration++; // Move to the next letter
-					}
-				}, animationIntervalDuration);
-			});
+			h1.addEventListener('mouseover', handleMouseOver);
 		}
+	}
+
+	function getRandomLetter(): string {
+		const randomIndex = Math.floor(Math.random() * letters.length);
+		return letters[randomIndex];
+	}
+
+	function handleMouseOver(event: MouseEvent): void {
+		const target = event.target as HTMLHeadingElement; // Type assertion to HTMLHeadingElement
+
+		if (target.classList.contains('animating')) {
+			return; // Exit if already animating
+		}
+
+		target.classList.add('animating'); // Add class to indicate animation
+
+		let iteration = 0;
+		let interval: ReturnType<typeof setInterval> | undefined = undefined;
+
+		if (interval) {
+			clearInterval(interval);
+		}
+
+		interval = setInterval(() => {
+			const targetValue = target.dataset.value!;
+			const currentText = target.innerText;
+			const nextText = Array.from(currentText)
+				.map((letter, index) => {
+					if (iteration === 0) {
+						return getRandomLetter();
+					} else if (index < iteration) {
+						return targetValue[index];
+					} else if (index === iteration && letter === targetValue[index]) {
+						return letter; // Stop generating for this letter
+					}
+
+					return getRandomLetter();
+				})
+				.join('');
+
+			target.innerText = nextText;
+
+			if (iteration >= targetValue.length) {
+				clearInterval(interval!);
+				target.classList.remove('animating'); // Remove class after animation
+			} else if (nextText[iteration] === targetValue[iteration]) {
+				iteration++; // Move to the next letter
+			}
+		}, animationIntervalDuration);
 	}
 
 	onMount(() => {
