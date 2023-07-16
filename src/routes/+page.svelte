a045a670654f2e626af0781a4c612700e4e0a78b
asyncedd <isynqquwu@proton.me>
2023-07-16 21:24:29 +0900

    feat: Make the skills headers more responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,14 @@
 	import Header from '../components/header.svelte';
 	import DividersPacman from '../components/dividers/pacman.svelte';
 	import SkillIssue from '../components/skills/skill_issue.svelte';
-	import LanguageIssue from '../components/skills/language_issue.svelte';
+  import LanguageIssue from '../components/skills/language_issue.svelte';
   import FrameworkIssue from '../components/skills/framework_issue.svelte';
-	import { theme } from '../components/navbar.svelte';
-	import '@fontsource/jetbrains-mono';
+  import { theme } from '../components/navbar.svelte';
+  import '@fontsource/jetbrains-mono';
 </script>
 
 <svelte:head>
-	<title>async</title>
+  <title>async</title>
 </svelte:head>
 
 <Navbar />
@@ -28,52 +28,48 @@
 <Hackerman />
 
 <body class="bg-ctp-base {$theme} scroll-smooth" style="overflow: hidden visible;">
-	<div class="bg-ctp-blue">
-		<Header />
-	</div>
-	<Wave />
-	<div class="p-[5rem]" />
-	<div class="flex items-center justify-center">
-		<div class="flex items-center justify-center p-[5rem] text-[5rem]">
-			<h1 data-value="Skills" class="w-[20rem] text-center">Skills</h1>
-		</div>
-	</div>
-	<div class="flex justify-center p-[3rem]">
-		<h1
-			data-value="Langs"
-			class="m-8 w-[17rem] text-[5rem]"
-			style="padding-right: 9999; padding-left: 9999;"
-		>
-			Langs
-		</h1>
+  <div class="bg-ctp-blue">
+    <Header />
+  </div>
+  <Wave />
+  <div class="p-[5rem]" />
+  <div class="flex items-center justify-center">
+    <div class="flex items-center justify-center p-[5rem] text-[5rem]">
+      <h1 data-value="Skills" class="w-[20rem] text-center">Skills</h1>
+    </div>
+  </div>
+  <div class="flex justify-center p-6 md:p-3 lg:p-1.5">
+    <h1
+      data-value="Langs"
+      class="m-2 md:m-4 lg:m-6 w-50 md:w-64 lg:w-80 text-4xl md:text-6xl lg:text-10xl"
+    >
+      Langs
+    </h1>
     <h1
       data-value="Frames"
-      class="m-8 w-[20rem] text-[5rem]"
-      style="padding-right: 9999; padding-left: 9999;"
-      >
+      class="m-2 md:m-4 lg:m-6 w-50 md:w-60 lg:w-80 text-4xl md:text-6xl lg:text-10xl"
+    >
       Frames
-		</h1>
-
-		<h1
-			data-value="Tools"
-			class="m-8 w-[17rem] text-[5rem]"
-			style="padding-right: 9999; padding-left: 9999;"
-		>
-			Tools
-		</h1>
-	</div>
-	<div class="flex justify-center">
-		<div class="p-16" style="padding-right: 1rem; padding-left: 1rem;">
-			<LanguageIssue />
-		</div>
-    <div class="p-16" style="padding-right: 3rem; padding-left: 3rem;">
+    </h1>
+    <h1
+      data-value="Tools"
+      class="m-2 md:m-4 lg:m-6 w-50 md:w-64 lg:w-80 text-4xl md:text-6xl lg:text-10xl"
+    >
+      Tools
+    </h1>
+  </div>
+  <div class="flex justify-center">
+    <div class="m-4 md:md-4 lg:m-6 p-4 md:p-8 lg:p-16" style="padding-right: 0.5rem; padding-left: 0.5rem;">
+      <LanguageIssue />
+    </div>
+    <div class="m-4 md:md-4 lg:m-6 p-4 md:p-8 lg:p-16" style="padding-right: 1rem; padding-left: 1rem;">
       <FrameworkIssue />
     </div>
-		<div class="p-16" style="padding-right: 1rem; padding-left: 1rem;">
-			<SkillIssue />
-		</div>
+    <div class="m-4 md:md-4 lg:m-6 p-4 md:p-8 lg:p-16" style="padding-right: 0.5rem; padding-left: 0.5rem;">
+      <SkillIssue />
+    </div>
   </div>
-	<div class="p-16" />
+  <div class="p-16" />
 	<DividersPacman />
 	<div class="flex items-center justify-center p-[3rem]">
 		<h1 data-value="Lorem" class="m-8 w-[20rem] text-[5rem]" style="white-space: nowrap;">Lorem</h1>
