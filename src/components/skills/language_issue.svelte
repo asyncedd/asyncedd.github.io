ada8a26a788f51c6411b77e074cf4f595fa605c0
asyncedd <isynqquwu@proton.me>
2023-07-16 19:48:20 +0900

    refactor: Optimize the languages issue

--- src/components/skills/language_issue.svelte
+++ src/components/skills/language_issue.svelte
@@ -1,82 +1,67 @@
-<div class="bento-box grid">
-	<div class="bento-box-item">
-		<img src="/rust.webp" alt="Rust" class="grow-on-hover" loading="lazy" decoding="async" />
-	</div>
-	<div class="bento-box-item">
-		<img src="/lua.webp" alt="Lua" class="grow-on-hover" loading="lazy" decoding="async" />
-	</div>
-	<div class="bento-box-item">
-		<img src="/css.webp" alt="CSS" class="grow-on-hover" loading="lazy" decoding="async" />
-	</div>
-	<div class="bento-box-item">
-		<img src="/html.webp" alt="HTML" class="grow-on-hover" loading="lazy" decoding="async" />
-	</div>
-	<div class="bento-box-item">
-		<img src="/typescript.webp" alt="Typescript" class="grow-on-hover" loading="lazy" decoding="async" />
-	</div>
-	<div class="bento-box-item">
-		<img
-			src="/javascript.webp"
-			alt="Javascript"
-			style="border-radius: 10px;"
-			class="the-worst-and-best-programming-language-of-all-time"
-			loading="lazy"
-			decoding="async"
-		/>
-	</div>
-	<div class="bento-box-item">
-		<img src="/C++.webp" alt="C++" class="grow-on-hover" loading="lazy" decoding="async" />
-	</div>
-  <div class="bento-box-item">
-    <img src="/C.webp" alt="C" class="grow-on-hover" loading="lazy" decoding="async" />
-  </div>
-  <div class="bento-box-item">
-    <img src="/go.webp" alt="go" class="grow-on-hover" loading="lazy" decoding="async" />
-  </div>
+<script>
+  const languages = [
+    { name: 'Rust', src: '/rust.webp' },
+    { name: 'Lua', src: '/lua.webp' },
+    { name: 'CSS', src: '/css.webp' },
+    { name: 'HTML', src: '/html.webp' },
+    { name: 'Typescript', src: '/typescript.webp' },
+    { name: 'JavaScript', src: '/javascript.webp', className: 'the-worst-and-best-programming-language-of-all-time', style: "border-radius: 10px" },
+    { name: 'C++', src: '/C++.webp' },
+    { name: 'C', src: '/C.webp' },
+    { name: 'Go', src: '/go.webp' },
+  ];
+</script>
+
+<div class="bento-box grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+  {#each languages as language}
+    <div class="bento-box-item">
+      <img src={language.src} alt={language.name} class="{language.className} grow-on-hover" style={language.style} loading="lazy" decoding="async" />
+    </div>
+  {/each}
 </div>
 
 <style>
-	.bento-box {
-		display: grid;
-		gap: 1rem;
-		grid-template-columns: repeat(3, auto);
-		grid-template-rows: repeat(2, auto);
-	}
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
 
-	.bento-box-item {
-		width: 100px;
-		height: 100px;
-		@apply bg-ctp-surface1;
-		border-radius: 30px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		transition: background-color 0.3s;
-	}
+@import url('https://rsms.me/inter/inter.css'); /* Import the Inter font */
 
-	.bento-box-item:hover {
-		@apply bg-ctp-surface2;
-	}
+/* Define your custom styles below */
+.bento-box-item {
+  width: 100px;
+  height: 100px;
+  @apply bg-ctp-surface1;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s;
+}
 
-	.bento-box-item .grow-on-hover {
-		transition: transform 0.3s;
-	}
+.bento-box-item:hover {
+  @apply bg-ctp-surface2;
+}
 
-	.bento-box-item .grow-on-hover:hover {
-		transform: scale(1.1);
-	}
+.bento-box-item .grow-on-hover {
+  transition: transform 0.3s;
+}
 
-	.bento-box-item img {
-		max-width: 100%;
-		max-height: 100%;
-	}
+.bento-box-item .grow-on-hover:hover {
+  transform: scale(1.1);
+}
 
-	.the-worst-and-best-programming-language-of-all-time:hover {
-		transform: scale(0.95);
-	}
+.bento-box-item img {
+  max-width: 100%;
+  max-height: 100%;
+}
 
-	.the-worst-and-best-programming-language-of-all-time {
-		transform: scale(0.85);
-		transition: transform 0.3s;
-	}
+.the-worst-and-best-programming-language-of-all-time:hover {
+  transform: scale(0.95);
+}
+
+.the-worst-and-best-programming-language-of-all-time {
+  transform: scale(0.85);
+  transition: transform 0.3s;
+}
 </style>
