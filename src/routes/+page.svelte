4dcd9bedf6b866089f80f7122ba7781a371c1e60
asyncedd <isynqquwu@proton.me>
2023-07-23 07:29:00 +0900

    feat: Add some styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
+<body class="bg-ctp-base">
+	<h1 class="text-ctp-text text-center text-[3rem]">async</h1>
+</body>
+
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+	href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
+	rel="stylesheet"
+/>
+
 <style>
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
+
+	* {
+		font-family: JetBrains Mono;
+	}
 </style>
