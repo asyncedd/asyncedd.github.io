05af62f6fb2b43154e7df143a2972de6ca1c65e8
asyncedd <isynqquwu@proton.me>
2023-07-24 09:42:45 +0900

    feat(typewriter): allow for multiple sentences

--- src/components/typewriter.svelte
+++ src/components/typewriter.svelte
@@ -1,23 +1,53 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let txt = 'GNU/Linux enjoyer'; /* The text */
+	let sentences = [
+		'GNU/Linux enjoyer',
+		'Coding enthusiast',
+		'Svelte fan',
+		'Welcome to my website!'
+	];
+
 	let speed = 50; /* The speed/duration of the effect in milliseconds */
 	let currentText = '';
+	let currentSentenceIndex = 0;
 	let isTyping = false;
 
 	function typeWriter() {
 		isTyping = true;
 		let i = 0;
+		const sentenceLength = sentences[currentSentenceIndex].length;
 		const interval = setInterval(() => {
-			if (i < txt.length) {
-				currentText += txt.charAt(i);
+			if (i < sentenceLength) {
+				currentText += sentences[currentSentenceIndex].charAt(i);
 				i++;
 			} else {
 				clearInterval(interval);
-				setTimeout(() => {
-					isTyping = false;
-				}, 3000);
+
+				// Start backspacing if not on the last iteration
+				if (currentSentenceIndex < sentences.length - 1) {
+					isTyping = true;
+					setTimeout(() => {
+						const backspaceInterval = setInterval(() => {
+							currentText = currentText.slice(0, -1);
+							if (currentText === '') {
+								clearInterval(backspaceInterval);
+								// Move to the next sentence
+								currentSentenceIndex++;
+								if (currentSentenceIndex < sentences.length) {
+									isTyping = false;
+									setTimeout(() => {
+										typeWriter(); // Start typing the next sentence
+									}, 1000); // Delay before starting the next sentence
+								}
+							}
+						}, speed / 2); // Adjust the backspace speed as per your preference
+					}, 1000); // Delay before starting the backspace
+				} else {
+					setTimeout(() => {
+						isTyping = false;
+					}, 5000);
+				}
 			}
 		}, speed);
 	}
