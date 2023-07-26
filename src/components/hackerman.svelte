b374135657752341bdb444c9e9a1f3b4ee78e692
asyncedd <isynqquwu@proton.me>
2023-07-27 07:07:30 +0900

    refactor(hackerman): init

--- src/components/hackerman.svelte
+++ src/components/hackerman.svelte
@@ -12,14 +12,11 @@
 	const animationFrameDuration = 30;
 
 	function getRandomLetter(targetValue, iteration, previousInterpretationCorrect) {
+		if (targetValue[iteration] === ' ') return ' ';
+
 		const randomValue = Math.random();
 		const randomIndex = Math.floor(Math.random() * lettersArray.length);
-		let letter = lettersArray[randomIndex];
-		const targetChar = targetValue[iteration];
-
-		if (targetChar === ' ') {
-			return ' ';
-		}
+		const letter = lettersArray[randomIndex];
 
 		if (
 			previousInterpretationCorrect &&
@@ -27,23 +24,64 @@
 			randomValue > 0.4 &&
 			iteration !== 0
 		) {
-			return targetChar;
+			return targetValue[iteration];
 		}
 
 		if (
 			iteration !== 0 &&
-			(letter.toUpperCase() === targetChar ||
-				letter.toLowerCase() === targetChar ||
-				targetChar === ' ')
+			(letter.toUpperCase() === targetValue[iteration] ||
+				letter.toLowerCase() === targetValue[iteration] ||
+				targetValue[iteration] === ' ')
 		) {
-			if (letter === letter.toUpperCase()) {
-				return letter.toLowerCase();
-			} else {
-				return letter.toUpperCase();
-			}
+			return letter === letter.toLowerCase() ? letter.toLowerCase() : letter.toUpperCase();
 		}
 
 		return letter;
+	}
+
+	function animateTarget(target, targetValue) {
+		const animationDuration = animationIntervalDuration * targetValue.length;
+		let iteration = 0;
+		let interval;
+		let previousInterpretationCorrect = true;
+
+		animating.set(true);
+		target.dataset.animating = 'true';
+
+		setTimeout(animate, animationDuration);
+
+		function animate() {
+			const currentText = target.textContent;
+			let nextText = '';
+
+			for (let i = 0; i < currentText.length; i++) {
+				if (iteration === 0) {
+					nextText += getRandomLetter(targetValue, iteration, previousInterpretationCorrect);
+				} else if (i < iteration || currentText[i] === targetValue[i]) {
+					nextText += targetValue[i];
+				} else {
+					previousInterpretationCorrect = false;
+					nextText += getRandomLetter(targetValue, iteration, previousInterpretationCorrect);
+				}
+			}
+
+			target.textContent = nextText;
+
+			if (iteration >= targetValue.length) {
+				target.dataset.animating = 'false';
+				animating.set(false);
+			} else if (nextText[iteration] === targetValue[iteration]) {
+				iteration++;
+				previousInterpretationCorrect = true;
+				setTimeout(() => {
+					interval = window.requestAnimationFrame(animate);
+				}, animationFrameDuration);
+			} else {
+				setTimeout(() => {
+					interval = window.requestAnimationFrame(animate);
+				}, animationFrameDuration);
+			}
+		}
 	}
 
 	function handleMouseOver(event) {
@@ -53,53 +91,7 @@
 			return;
 		}
 
-		animating.set(true);
-		target.dataset.animating = 'true';
-
-		let iteration = 0;
-		let interval;
-		let previousInterpretationCorrect = true; // New variable to track the correctness of the previous interpretation
-
-		setTimeout(() => {
-			animate();
-		}, animationIntervalDuration);
-
-		function animate() {
-			const targetValue = target.dataset.value;
-			const currentText = target.textContent;
-			let nextText = '';
-
-			for (let i = 0; i < currentText.length; i++) {
-				const currentChar = currentText[i];
-				if (iteration === 0) {
-					nextText += getRandomLetter(targetValue, iteration, previousInterpretationCorrect);
-				} else if (i < iteration) {
-					nextText += targetValue[i];
-				} else if (currentChar === targetValue[i]) {
-					nextText += targetValue[i];
-				} else {
-					previousInterpretationCorrect = false;
-					nextText += getRandomLetter(targetValue, iteration, previousInterpretationCorrect);
-				}
-			}
-
-			target.textContent = nextText;
-
-			if (iteration >= targetValue.length) {
-				target.dataset.animating = 'false';
-				animating.set(false);
-			} else if (nextText[iteration] === targetValue[iteration]) {
-				iteration++;
-				previousInterpretationCorrect = true; // Reset to true for the next iteration
-				setTimeout(() => {
-					interval = window.requestAnimationFrame(animate);
-				}, animationFrameDuration);
-			} else {
-				setTimeout(() => {
-					interval = window.requestAnimationFrame(animate);
-				}, animationFrameDuration);
-			}
-		}
+		animateTarget(target, target.dataset.value);
 	}
 
 	onMount(() => {
@@ -107,58 +99,7 @@
 		for (const h1 of h1Elements) {
 			h1.addEventListener('mouseover', handleMouseOver);
 			h1.dataset.animating = 'false';
-			animateH1Element(h1);
+			animateTarget(h1, h1.dataset.value);
 		}
 	});
-
-	function animateH1Element(target) {
-		const targetValue = target.dataset.value;
-		const animationDuration = animationIntervalDuration * targetValue.length;
-
-		animating.set(true);
-		target.dataset.animating = 'true';
-
-		let iteration = 0;
-		let interval;
-		let previousInterpretationCorrect = true; // New variable to track the correctness of the previous interpretation
-
-		setTimeout(() => {
-			animate();
-		}, animationDuration);
-
-		function animate() {
-			const currentText = target.textContent;
-			let nextText = '';
-
-			for (let i = 0; i < currentText.length; i++) {
-				if (iteration === 0) {
-					nextText += getRandomLetter(targetValue, iteration, previousInterpretationCorrect);
-				} else if (i < iteration) {
-					nextText += targetValue[i];
-				} else if (i === iteration && currentText[i] === targetValue[i]) {
-					nextText += targetValue[i];
-				} else {
-					previousInterpretationCorrect = false;
-					nextText += getRandomLetter(targetValue, iteration, previousInterpretationCorrect);
-				}
-			}
-
-			target.textContent = nextText;
-
-			if (iteration >= targetValue.length) {
-				target.dataset.animating = 'false';
-				animating.set(false);
-			} else if (nextText[iteration] === targetValue[iteration]) {
-				iteration++;
-				previousInterpretationCorrect = true; // Reset to true for the next iteration
-				setTimeout(() => {
-					interval = window.requestAnimationFrame(animate);
-				}, animationFrameDuration);
-			} else {
-				setTimeout(() => {
-					interval = window.requestAnimationFrame(animate);
-				}, animationFrameDuration);
-			}
-		}
-	}
 </script>
