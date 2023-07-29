d7810a1742bf2dfae349f3f9ed69f499a92c7ea9
asyncedd <isynqquwu@proton.me>
2023-07-30 07:29:36 +0900

    fix(tailwindcss): Remove deprecated options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	mode: 'jit',
-	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
