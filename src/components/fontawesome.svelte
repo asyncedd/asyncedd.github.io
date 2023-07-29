ee15f989317a8e32bfe1c4832c00b345044e8215
asyncedd <isynqquwu@proton.me>
2023-07-29 15:33:58 +0900

    fix: Try to fix invisible icons
    fix: Font-awesome

--- src/components/fontawesome.svelte
+++ src/components/fontawesome.svelte
@@ -1,3 +1,1 @@
-<head>
-	<script src="https://kit.fontawesome.com/777ef29ec5.js" crossorigin="anonymous"></script>
-</head>
+<script src="https://kit.fontawesome.com/777ef29ec5.js" crossorigin="anonymous"></script>
