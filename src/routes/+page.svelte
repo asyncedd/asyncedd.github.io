d3a63644c48a065a698392cc84f3e8eef62611c4
asyncedd <isynqquwu@proton.me>
2023-07-30 14:20:01 +0900

    fix: Improve mobile compatibility
    fix
    
    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,28 +21,35 @@
 	<div class="dark:bg-ctp-base bg-ctp-blue">
 		<Navbar />
 	</div>
-	<div class="py-[40vh] dark:bg-ctp-base bg-ctp-blue">
-		<div class="flex justify-center dark:bg-ctp-base bg-ctp-blue">
-			<div class="h-[20vh] overflow-none">
-				<div class="inline-flex mx-[3vw]">
-					<img src="moutains.webp" alt="" class="h-[17vh] rounded-[100%] aspect-square rotate" />
-					<h1
-						class="dark:text-ctp-text text-ctp-base text-center text-[7vh] ml-4 p-[3vh]"
-						class:animating={$animating === true}
-						data-value="async"
-					>
-						async
-					</h1>
+	<div class="py-[30vh] sm:py-[40vh] dark:bg-ctp-base bg-ctp-blue items-center content-center">
+		<div
+			class="flex flex-col sm:flex-row dark:bg-ctp-base bg-ctp-blue mx-[3vw] space-x-4 justify-center items-center sm:items-start"
+		>
+			<div class="order-last sm:order-first h-[20vh] w-full sm:w-auto overflow-none">
+				<div class="min-w-[15vw]">
+					<div class="inline-flex items-center justify-center h-full w-full pt-[3vh] sm:pt-0">
+						<h1
+							class="dark:text-ctp-text text-ctp-base text-center text-[5rem] px-4"
+							class:animating={$animating === true}
+							data-value="async"
+						>
+							async
+						</h1>
+					</div>
+					<div class="pt-[1rem] dark:bg-ctp-base bg-ctp-blue">
+						<div class="flex justify-center">
+							<div class="h1-container text-[1.3rem] text-ctp-base dark:text-ctp-text">
+								<Typewriter sentences={externalSentences} />
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="pt-[1rem] dark:bg-ctp-base bg-ctp-blue">
-			<div class="flex justify-center">
-				<div class="h1-container text-[1.3rem] text-ctp-base dark:text-ctp-text">
-					<Typewriter sentences={externalSentences} />
-				</div>
+			<div class="items-center justify-center h-[20vh] sm:order-last">
+				<img src="moutains.webp" alt="" class="h-full rounded-[100%] aspect-square rotate" />
 			</div>
 		</div>
+		<div class="pb-[13vh]" />
 	</div>
 	<div style="transform: rotate(-180deg);">
 		<Wave />
@@ -63,7 +70,6 @@
 		</h2>
 	</footer>
 </body>
-
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" rossorigin />
 <link
@@ -87,7 +93,7 @@
 	}
 
 	h1:hover {
-		@apply text-ctp-green text-[7.5vh];
+		@apply text-ctp-green text-[6rem];
 	}
 
 	.h1-container {
