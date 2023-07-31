77363a74564b0aefc51f19b04f8608c1eca084e9
asyncedd <isynqquwu@proton.me>
2023-08-01 07:04:01 +0900

    fix(partytown): Remove

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import { defineConfig } from 'vite';
 import { partytownVite } from '@builder.io/partytown/utils';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		partytownVite({
-			// `dest` specifies where files are copied to in production
-			dest: join(process.cwd(), 'static', '~partytown')
-		})
-	]
+	plugins: [sveltekit()]
 });
