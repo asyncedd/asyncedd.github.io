f54c5bad0b0753bca3c6ed766977029c70ef93c9
asyncedd <isynqquwu@proton.me>
2023-08-01 07:09:18 +0900

    fix: Fix build errors

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { partytownVite } from '@builder.io/partytown/utils';
 
 export default defineConfig({
 	plugins: [sveltekit()]
