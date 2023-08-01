5094bb6030daff01f1482fdf06a1661e4c4e4ea0
asyncedd <isynqquwu@proton.me>
2023-08-02 07:34:52 +0900

    feat(lorem): Mkae it look better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,13 +59,14 @@
 		<Wave />
 	</div>
 	<div class="p-[30vh] text-center px-[10vw] dark:text-ctp-text text-ctp-base">
-		<i>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</i>
+		<p class="italic">
+			<span class="font-bold mx-1 text-[2rem]" , style="initial-letter: 3 2;">L</span>orem ipsum
+			dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+			dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+			ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+			esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+			sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
 	</div>
 	<Wave />
 	<footer class="dark:bg-ctp-base bg-ctp-blue">
