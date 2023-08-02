66cdaf487f57d752455bd85d80eee13325be0756
asyncedd <isynqquwu@proton.me>
2023-08-03 08:08:48 +0900

    fix: Fix jumpy italic text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -59,13 +59,13 @@
 	</div>
 	<div class="text-ctp-base dark:text-ctp-text">
 		<div class="py-[30vh] text-center px-[10vw]">
-			<i>
+			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
 				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
 				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</i>
+			</p>
 		</div>
 		<Wave />
 		<footer class="dark:bg-ctp-base bg-ctp-blue">
