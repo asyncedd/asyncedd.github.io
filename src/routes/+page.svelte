da55878c9bd7874d1eaedfcb35c0225a86ca7761
asyncedd <isynqquwu@proton.me>
2023-08-07 15:08:08 +0900

    fix: Fix the link :hover color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,13 +97,13 @@
 			<h2 class="text-center text-[1rem] p-[1rem]">Made with ❤️ by asyncedd</h2>
 			<p class="text-center text-[0.9rem] p-[0.3rem]">
 				The main image is shot by <a
-					class="dark:text-ctp-blue text-ctp-rosewater hover:text-ctp-sky transition-colors duration-150"
+					class="dark:text-ctp-blue text-ctp-rosewater hover:text-ctp-sky hover:dark:text-ctp-sky transition-colors duration-150"
 					href="https://unsplash.com/@romello?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
 					>Romello Williams</a
 				>
 				on
 				<a
-					class="dark:text-ctp-blue text-ctp-rosewater hover:text-ctp-sky transition-colors duration-150"
+					class="dark:text-ctp-blue text-ctp-rosewater hover:text-ctp-sky hover:dark:text-ctp-sky transition-colors duration-150"
 					href="https://unsplash.com/photos/P8VMwYFY-Es?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
 					>Unsplash</a
 				>
