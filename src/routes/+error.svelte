49451117cd72b1008911e301626ddda8a9e7cd50
asyncedd <isynqquwu@proton.me>
2023-08-09 14:39:45 +0900

    fix(+error): Fix broken icons

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -40,9 +40,17 @@
 				</div>
 			</div>
 
-			<div class="text-center mt-4 dark:text-ctp-text text-ctp-base typewriter-container">
-				<a class=" home-sweet-home text-[1.3rem]" href="/"
-					><i class="fa-solid fa-house" /> Go back</a
+			<div
+				class="text-center mt-4 dark:text-ctp-text text-ctp-base dark:fill-ctp-text fill-ctp-base hover:text-ctp-green hover:fill-ctp-green typewriter-container"
+			>
+				<a
+					class=" transition-colors duration-[0.5s] ease-in-out text-[1.3rem] inline-flex gap-3 hover:text-ctp-green hover:fill-ctp-green"
+					href="/"
+					><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
+						><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+							d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
+						/></svg
+					> Go back</a
 				>
 			</div>
 		</div>
