bb44c6629a54a534b0db62aef0c441f42c16bb9b
asyncedd <isynqquwu@proton.me>
2023-08-12 16:09:04 +0900

    perf: Optimize sub-optimial code

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,27 +9,28 @@
 </svelte:head>
 
 <!-- Posts -->
-<section class="h-[100vh] flex justify-center align-middle items-center">
-	<div class="flex align-middle items-center justify-center">
+<section class="h-[100vh] flex justify-center items-center">
+	<div class="flex items-center justify-center">
 		<div class="posts">
-			<h1 class="text-[5rem] border-b-content border-b-[1px] border-b-solid">BLOGS</h1>
+			<h1 class="text-5xl border-b-[1px] border-b-solid">BLOGS</h1>
 			{#each data.posts as post}
 				{#if !post.unlist}
 					<div class="post">
 						<a
-							href="blog/{post.slug}"
+							href={`blog/${post.slug}`}
 							class="title text-content hover:text-sky-300 hover:dark:text-sky-200 transition-colors duration-[0.5s] ease-in-out"
-							>{post.title}</a
 						>
-						<div class="tags my-[3px]">
+							{post.title}
+						</a>
+						<div class="tags my-3">
 							{#each post.categories as category}
-								<span class="bg-red-300 text-zinc-900 p-[5px] rounded-[0.5rem] gap-[10px] mx-[5px]"
-									>&num;{category}</span
-								>
+								<span class="bg-red-300 text-zinc-900 p-2 rounded-[0.5rem] gap-2 mx-1">
+									&num;{category}
+								</span>
 							{/each}
 						</div>
-						<p class="dark:text-zinc-50 text-zinc-900">{formatDate(post.date)}</p>
-						<p class="description">{post.description}</p>
+						<p class="text-content_dark">{formatDate(post.date)}</p>
+						<p class="description text-content">{post.description}</p>
 					</div>
 				{/if}
 			{/each}
