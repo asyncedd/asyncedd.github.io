c7234323fe59ca4515c59c1e83fe04804bc46730
asyncedd <isynqquwu@proton.me>
2023-08-13 11:03:41 +0900

    perf: Minimize the hackerman

--- src/components/hackerman.svelte
+++ src/components/hackerman.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <script lang="ts">
-	const lettersArray: string[] =
+	const lettersArray =
 		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()|'.split('');
 
 	let interval: undefined | any = null;
@@ -13,22 +13,16 @@
 	function animateH1Element(target: HTMLElement, targetValue: string) {
 		let iteration = 0;
 
-		if (target.dataset.animating != 'true') {
+		if (target.dataset.animating !== 'true') {
 			clearInterval(interval);
-
 			target.dataset.animating = 'true';
 			animating.set(true);
 
 			interval = setInterval(() => {
-				target.innerText = target.innerText
-					.split('')
-					.map((_, index) => {
-						if (index < iteration) {
-							return targetValue[index];
-						}
-
-						return lettersArray[Math.floor(Math.random() * 26)];
-					})
+				target.innerText = [...targetValue]
+					.map((_, index) =>
+						index < iteration ? targetValue[index] : lettersArray[Math.floor(Math.random() * 26)]
+					)
 					.join('');
 
 				if (iteration >= targetValue.length) {
@@ -43,13 +37,13 @@
 	}
 
 	function handleMouseOver(event: MouseEvent): void {
-		const target: HTMLElement = event.target as HTMLElement;
-		const targetValue: string = target.dataset.value || '';
+		const target = event.target as HTMLElement;
+		const targetValue = target.dataset.value || '';
 		animateH1Element(target, targetValue);
 	}
 
-	const h1Elements: NodeListOf<HTMLHeadingElement> = document.querySelectorAll('h1');
-	h1Elements.forEach((h1: HTMLHeadingElement) => {
+	const h1Elements = document.querySelectorAll('h1');
+	h1Elements.forEach((h1) => {
 		h1.addEventListener('mouseover', handleMouseOver);
 		h1.dataset.animating = 'false';
 		animateH1Element(h1, h1.dataset.value || '');
