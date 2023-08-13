81064b9fd134a6dc7d662ba727b6a98375dadf67
asyncedd <isynqquwu@proton.me>
2023-08-13 21:35:15 +0900

    perf: Use LightningCSS instead of CSSNano

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import lightningcss from 'vite-plugin-lightningcss';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		lightningcss({
+			browserslist: '>= 0.25%'
+		})
+	]
 });
