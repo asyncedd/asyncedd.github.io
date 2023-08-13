32f76803c741ccd0ee0c53554c7b82766b401929
asyncedd <isynqquwu@proton.me>
2023-08-13 17:33:43 +0900

    fix(SEO): Put the wave into the <footer> element

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -2,20 +2,22 @@
 	import Wave from '../components/wave.svelte';
 </script>
 
-<Wave />
-<footer class="bg-primary text-center">
-	<h2 class="text-center text-[1rem] p-[1rem]">Made with ❤️ by asyncedd</h2>
-	<p class="text-center text-[0.9rem] p-[0.3rem]">
-		The main image is shot by <a
-			class="text-blue-400 hover:text-sky-300 hover:dark:text-sky-400"
-			href="https://unsplash.com/@romello?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-			>Romello Williams</a
-		>
-		on
-		<a
-			class="text-blue-400 hover:text-sky-300 hover:dark:text-sky-400"
-			href="https://unsplash.com/photos/P8VMwYFY-Es?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-			>Unsplash</a
-		>
-	</p>
+<footer>
+	<Wave />
+	<div class="bg-primary text-center">
+		<h2 class="text-center text-[1rem] p-[1rem]">Made with ❤️ by asyncedd</h2>
+		<p class="text-center text-[0.9rem] p-[0.3rem]">
+			The main image is shot by <a
+				class="text-blue-400 hover:text-sky-300 hover:dark:text-sky-400"
+				href="https://unsplash.com/@romello?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+				>Romello Williams</a
+			>
+			on
+			<a
+				class="text-blue-400 hover:text-sky-300 hover:dark:text-sky-400"
+				href="https://unsplash.com/photos/P8VMwYFY-Es?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+				>Unsplash</a
+			>
+		</p>
+	</div>
 </footer>
