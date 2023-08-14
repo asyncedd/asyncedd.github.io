3a4cda168688a286b54ad83260b7c763b5bd05b0
asyncedd <isynqquwu@proton.me>
2023-08-15 08:05:26 +0900

    Revert "perf(hackerman): Fix LCP"
    This reverts commit f1561f9c61404898b7534909340c667be5fc8b8f.

--- src/components/hackerman.svelte
+++ src/components/hackerman.svelte
@@ -68,6 +68,7 @@
 		const targetValue = h1.dataset.value || '';
 		if (targetValue) {
 			h1.addEventListener('mouseover', handleMouseOver);
+			animateH1Element(h1, targetValue);
 		}
 	});
 </script>
