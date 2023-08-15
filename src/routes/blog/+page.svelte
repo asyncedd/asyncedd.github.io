dae2858492a63a206d7526ca0ee57c8e3a295761
asyncedd <isynqquwu@proton.me>
2023-08-15 09:19:55 +0900

    fix: Remove unused unlist checks

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -14,25 +14,23 @@
 		<div class="posts">
 			<h1 class="text-5xl border-b-[1px] border-b-content_dark">BLOGS</h1>
 			{#each data.posts as post}
-				{#if !post.unlist}
-					<div class="post">
-						<a
-							href={`blog/${post.slug}`}
-							class="title text-content hover:text-sky-300 hover:dark:text-sky-200 transition-colors duration-[0.5s] ease-in-out"
-						>
-							{post.title}
-						</a>
-						<div class="tags my-3">
-							{#each post.categories as category}
-								<span class="bg-red-300 text-zinc-900 p-2 rounded-[0.5rem] gap-2 mx-1">
-									&num;{category}
-								</span>
-							{/each}
-						</div>
-						<p class="text-content_dark">{formatDate(post.date)}</p>
-						<p class="description text-content">{post.description}</p>
+				<div class="post">
+					<a
+						href={`blog/${post.slug}`}
+						class="title text-content hover:text-sky-300 hover:dark:text-sky-200 transition-colors duration-[0.5s] ease-in-out"
+					>
+						{post.title}
+					</a>
+					<div class="tags my-3">
+						{#each post.categories as category}
+							<span class="bg-red-300 text-zinc-900 p-2 rounded-[0.5rem] gap-2 mx-1">
+								&num;{category}
+							</span>
+						{/each}
 					</div>
-				{/if}
+					<p class="text-content_dark">{formatDate(post.date)}</p>
+					<p class="description text-content">{post.description}</p>
+				</div>
 			{/each}
 		</div>
 	</div>
