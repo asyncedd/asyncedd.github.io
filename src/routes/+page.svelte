3ab2ec45398ac9a057bda8acd1ad8edd6a340147
asyncedd <isynqquwu@proton.me>
2023-08-16 21:07:27 +0900

    fix(+page): Fix typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,13 +73,14 @@
 <div style="transform: rotate(-180deg);">
 	<Wave />
 </div>
-<p class="py-[30vh] text-center px-[10vw]">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-	labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-	nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-	esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-	in culpa qui officia deserunt mollit anim id est laborum.
-</p>
+<div class="flex justify-center">
+	<p class="py-[30vh] text-left w-[50vw] indent-5">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+	</p>
+</div>
 
 {#if hanime}
 	<svelte:component this={Hackerman} />
