51c4f23dbd6f42d78e23df836b24b5a947e4d395
asyncedd <isynqquwu@proton.me>
2023-08-18 21:48:45 +0900

    fix(footer): Make the footer use accent colors for the theme

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -8,13 +8,13 @@
 		<h2 class="text-center text-[1rem] p-[1rem]">Made with ❤️ by asyncedd</h2>
 		<p class="text-center text-[0.9rem] p-[0.3rem]">
 			The main image is shot by <a
-				class="text-blue-400 hover:text-sky-300 hover:dark:text-sky-400"
+				class="text-accent hover:underline"
 				href="https://unsplash.com/@romello?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
 				>Romello Williams</a
 			>
 			on
 			<a
-				class="text-blue-400 hover:text-sky-300 hover:dark:text-sky-400"
+				class="text-accent hover:underline"
 				href="https://unsplash.com/photos/P8VMwYFY-Es?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
 				>Unsplash</a
 			>
