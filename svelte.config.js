ffb36df2fe59a9148bc7f6f6fe384de1b0cd988e
asyncedd <isynqquwu@proton.me>
2023-08-21 11:37:35 +0900

    perf: Use the static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import rehypeSlug from 'rehype-slug';
 import remarkToc from 'remark-toc';
