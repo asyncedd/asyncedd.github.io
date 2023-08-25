566286ca2471d43a351f295dac2334de57d8153f
asyncedd <isynqquwu@proton.me>
2023-08-26 06:59:29 +0900

    refactor(waves): Inline CSS

--- src/lib/components/wave.svelte
+++ src/lib/components/wave.svelte
@@ -14,59 +14,54 @@
 					d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
 				/>
 			</defs>
-			<g class="parallax">
-				<use
-					xlink:href="#gentle-wave"
-					x="48"
-					y="0"
-					fill="var(--wave-color-1)"
-					style="animation-delay: -2s;"
+			<use xlink:href="#gentle-wave" x="48" y="0" class="fill-accent/30">
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					dur="25s"
+					values="-90 0; 85 0"
+					keyTimes="0; 1"
+					repeatCount="indefinite"
+					begin="-2s"
+					calcMode="linear"
 				/>
-				<use
-					xlink:href="#gentle-wave"
-					x="48"
-					y="3"
-					fill="var(--wave-color-2)"
-					style="animation-delay: -3s;"
+			</use>
+			<use xlink:href="#gentle-wave" x="48" y="3" class="fill-accent/50">
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					dur="25s"
+					values="-90 0; 85 0"
+					keyTimes="0; 1"
+					repeatCount="indefinite"
+					begin="-3s"
+					calcMode="linear"
 				/>
-				<use
-					xlink:href="#gentle-wave"
-					x="48"
-					y="5"
-					fill="var(--wave-color-3)"
-					style="animation-delay: -4s;"
+			</use>
+			<use xlink:href="#gentle-wave" x="48" y="5" class="fill-primary/70">
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					dur="25s"
+					values="-90 0; 85 0"
+					keyTimes="0; 1"
+					repeatCount="indefinite"
+					begin="-4s"
+					calcMode="linear"
 				/>
-				<use
-					xlink:href="#gentle-wave"
-					x="48"
-					y="7"
-					fill="var(--wave-color-4)"
-					style="animation-delay: -5s;"
+			</use>
+			<use xlink:href="#gentle-wave" x="48" y="7" class="fill-primary">
+				<animateTransform
+					attributeName="transform"
+					type="translate"
+					dur="25s"
+					values="-90 0; 85 0"
+					keyTimes="0; 1"
+					repeatCount="indefinite"
+					begin="-5s"
+					calcMode="linear"
 				/>
-			</g>
+			</use>
 		</svg>
 	</div>
 </div>
-
-<style lang="postcss" async>
-	:root {
-		--wave-color-1: theme('colors.accent/0.3');
-		--wave-color-2: theme('colors.accent/0.5');
-		--wave-color-3: theme('colors.primary/0.7');
-		--wave-color-4: theme('colors.primary');
-	}
-
-	/* Animation */
-	.parallax > use {
-		animation: move-forever 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
-	}
-
-	@keyframes move-forever {
-		0% {
-			transform: translate3d(-90px, 0, 0);
-		}
-		100% {
-			transform: translate3d(85px, 0, 0);
-		}
-	}
-</style>
