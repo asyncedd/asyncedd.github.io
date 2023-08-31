ff16911c35caef3089eb610ac7c2ab1ff54cf39e
asyncedd <isynqquwu@proton.me>
2023-09-01 07:58:47 +0900

    Revert "fix(header): Fix SEO/a11y/ARIA problems"
    This reverts commit 7c9652c962dd83cb63caec2f8154831da1c2155e.

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -16,26 +16,24 @@
 	];
 </script>
 
-<header class="w-[100%] h-[3rem] flex">
+<nav class="w-[100%] h-[3rem] flex">
 	<div
 		class="w-[100%] h-[3rem] inline-flex items-center bg-background_dark/50 saturate-150 backdrop-blur-[17px] text-foreground justify-between p-[clamp(0.5rem,0.5rem+3vw,3rem)] px-[clamp(0.3rem,0.3rem+10vw,10%)] fixed"
 	>
-		<nav>
-			<a href="/" class="inline-flex items-center gap-3 group">
-				<div
-					class="h-10 w-10 {$page.url.pathname === '/'
-						? 'bg-foreground'
-						: 'bg-foreground/75'} motion-safe:group-hover:bg-foreground rounded-[10rem] transition-[background] duration-[0.25s] ease-in-out"
-				/>
-				<b
-					class="text-[clamp(0.7rem,_1vw+0.7rem,_1.5rem)] tracking-widest motion-safe:group-hover:text-foreground transition-[color] duration-[0.25s] ease-in-out {$page
-						.url.pathname === '/'
-						? 'text-foreground'
-						: 'text-foreground/75'}">/ async</b
-				></a
-			>
-			<Links {links} />
-		</nav>
+		<a href="/" class="inline-flex items-center gap-3 group">
+			<div
+				class="h-10 w-10 {$page.url.pathname === '/'
+					? 'bg-foreground'
+					: 'bg-foreground/75'} motion-safe:group-hover:bg-foreground rounded-[10rem] transition-[background] duration-[0.25s] ease-in-out"
+			/>
+			<b
+				class="text-[clamp(0.7rem,_1vw+0.7rem,_1.5rem)] tracking-widest motion-safe:group-hover:text-foreground transition-[color] duration-[0.25s] ease-in-out {$page
+					.url.pathname === '/'
+					? 'text-foreground'
+					: 'text-foreground/75'}">/ async</b
+			></a
+		>
+		<Links {links} />
 		<Themeswitch button_name="Theme" optionList={['dark', 'light']} />
 	</div>
-</header>
+</nav>
