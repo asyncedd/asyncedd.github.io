e7d4932ed6d456d51c370fcf92f283d252d39350
asyncedd <isynqquwu@proton.me>
2023-09-10 10:31:16 +0900

    feat: Make the dark mode more grayish
    * fix consistency
    
    * fix

--- src/lib/pages/lister.svelte
+++ src/lib/pages/lister.svelte
@@ -52,7 +52,7 @@
 			</h1>
 			<div class="relative mb-4 w-full">
 				<input
-					class="block w-full rounded-md bg-foreground_dark/25 px-4 py-2 font-bold text-foreground_dark placeholder:text-foreground_dark/50 focus:outline-none"
+					class="block w-full rounded-md bg-foreground_dark/[.15] px-4 py-2 font-bold text-foreground_dark placeholder:text-foreground_dark/50 focus:outline-none"
 					type="text"
 					id="name"
 					name="name"
@@ -65,9 +65,8 @@
 					width="32"
 					height="32"
 					viewBox="0 0 32 32"
-					class="absolute right-3 top-2.5 h-5 w-5 text-gray-400"
+					class="absolute right-3 top-2.5 h-5 w-5 dark:fill-gray-300 fill-gray-900"
 					><path
-						fill="currentColor"
 						d="M19 3C13.488 3 9 7.488 9 13c0 2.395.84 4.59 2.25 6.313L3.281 27.28l1.439 1.44l7.968-7.969A9.922 9.922 0 0 0 19 23c5.512 0 10-4.488 10-10S24.512 3 19 3zm0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8s-8-3.57-8-8s3.57-8 8-8z"
 					/></svg
 				>
