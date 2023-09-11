e929683b8069dc4e77f93b4d23d58f4afffa40ce
asyncedd <isynqquwu@proton.me>
2023-09-11 17:58:16 +0900

    fix(dropdownmenu): Fix the margin-top

--- src/lib/components/header/menu.svelte
+++ src/lib/components/header/menu.svelte
@@ -61,7 +61,7 @@
 				out:fly={{
 					y: -15
 				}}
-				class="absolute -ml-[3rem] w-[7rem] flex-col rounded-[0.5rem] border-[1px] border-background_lighter bg-background_dark"
+				class="absolute -ml-[3rem] mt-[0.5rem] w-[7rem] flex-col rounded-[0.5rem] border-[1px] border-background_lighter bg-background_dark"
 				id="theme-select"
 			>
 				<li class="flex border-b-[1px] border-b-background_lighter p-[2px] last:border-b-0">
--- src/lib/components/header/themeswitcher.svelte
+++ src/lib/components/header/themeswitcher.svelte
@@ -89,7 +89,7 @@
 			out:fly={{
 				y: -15
 			}}
-			class="absolute -ml-[3rem] w-[7rem] flex-col rounded-[0.5rem] border-[1px] border-background_lighter bg-background_dark"
+			class="absolute -ml-[3rem] mt-[0.5rem] w-[7rem] flex-col rounded-[0.5rem] border-[1px] border-background_lighter bg-background_dark"
 			id="theme-select"
 		>
 			{#each optionList as option}
