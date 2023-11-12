c7c094251eb9e37e7f6ce3b3b4c75ca15144efcf
asyncedd <isynqquwu@proton.me>
2023-11-12 13:10:22 +0900

    feat: Redesign the theme buttons

--- src/lib/components/header/themeswitcher.svelte
+++ src/lib/components/header/themeswitcher.svelte
@@ -15,6 +15,16 @@
 	export let optionList = [];
 	export const button_name = '';
 
+	function switchTheme(e) {
+		if (e.target.checked) {
+			document.documentElement.setAttribute('data-theme', 'dark');
+			localStorage.setItem('theme', 'dark'); //add this
+		} else {
+			document.documentElement.setAttribute('data-theme', 'light');
+			localStorage.setItem('theme', 'light'); //add this
+		}
+	}
+
 	browser &&
 		(() => {
 			/**
@@ -48,20 +58,25 @@
 		})();
 </script>
 
-<div class="group">
-	<button
-		class="dropdown-button-menu-theme rounded-[0.5rem] p-[12px] ring-foreground hover:bg-background_light focus:ring active:ring"
-		aria-label="theme switcher"
-		on:click={() => {
-			dropdownOpened = !dropdownOpened;
-		}}
+<button
+	class="group inline-flex rounded-full border-[3px] border-zinc-300 p-[0.3rem] dark:border-zinc-700"
+	on:click={() => {
+		localStorage.theme =
+			currentTheme =
+			document.documentElement.className =
+				currentTheme == 'dark' ? 'light' : 'dark';
+	}}
+>
+	<div
+		aria-label="set theme to light"
+		class="inline-flex w-[100%] items-center justify-between rounded-full bg-zinc-300 p-3 text-left text-[1.1rem] capitalize tracking-wide dark:bg-background/30"
 	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="1em"
 			height="1em"
 			viewBox="0 0 32 32"
-			class="flex items-center justify-center stroke-foreground dark:hidden"
+			class="flex items-center justify-center stroke-foreground"
 			><path
 				fill="none"
 				stroke-linecap="round"
@@ -70,34 +85,23 @@
 				d="M16 6V3M8.929 8.929L6.808 6.808M6 16H3m13 13v-3m9.192-.808l-2.121-2.12M29 16h-3M8.929 23.071l-2.121 2.121M25.192 6.808l-2.12 2.121M22 16a6 6 0 1 1-12 0a6 6 0 0 1 12 0Z"
 			/></svg
 		>
+	</div>
+	<div
+		aria-label="set theme to dark"
+		class="inline-flex w-[100%] items-center justify-between rounded-full bg-background_dark/50 bg-background_light p-3 text-left text-[1.1rem] capitalize tracking-wide dark:bg-zinc-700"
+	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="1em"
 			height="1em"
-			viewBox="0 0 15 15"
-			class="hidden items-center justify-center stroke-foreground dark:flex"
+			viewBox="0 0 24 24"
+			class="flex items-center justify-center fill-foreground"
 			><path
 				fill-rule="evenodd"
-				d="M2.9.5a.4.4 0 0 0-.8 0v.6h-.6a.4.4 0 1 0 0 .8h.6v.6a.4.4 0 1 0 .8 0v-.6h.6a.4.4 0 0 0 0-.8h-.6V.5Zm3 3a.4.4 0 1 0-.8 0v.6h-.6a.4.4 0 1 0 0 .8h.6v.6a.4.4 0 1 0 .8 0v-.6h.6a.4.4 0 0 0 0-.8h-.6v-.6Zm-4 3a.4.4 0 1 0-.8 0v.6H.5a.4.4 0 1 0 0 .8h.6v.6a.4.4 0 0 0 .8 0v-.6h.6a.4.4 0 0 0 0-.8h-.6v-.6ZM8.544.982l-.298-.04c-.213-.024-.34.224-.217.4A6.57 6.57 0 0 1 9.203 5.1a6.602 6.602 0 0 1-6.243 6.59c-.214.012-.333.264-.183.417a6.8 6.8 0 0 0 .21.206l.072.066l.26.226l.188.148l.121.09l.187.131l.176.115c.12.076.244.149.37.217l.264.135l.26.12l.303.122l.244.086a6.568 6.568 0 0 0 1.103.26l.317.04l.267.02a6.6 6.6 0 0 0 6.943-7.328l-.037-.277a6.557 6.557 0 0 0-.384-1.415l-.113-.268l-.077-.166l-.074-.148a6.602 6.602 0 0 0-.546-.883l-.153-.2l-.199-.24l-.163-.18l-.12-.124l-.16-.158l-.223-.2l-.32-.26l-.245-.177l-.292-.19l-.321-.186l-.328-.165l-.113-.052l-.24-.101l-.276-.104l-.252-.082l-.325-.09l-.265-.06l-.292-.053Zm1.86 4.318a7.578 7.578 0 0 0-.572-2.894a5.601 5.601 0 1 1-4.748 10.146a7.61 7.61 0 0 0 3.66-2.51a.749.749 0 0 0 1.355-.442a.75.75 0 0 0-.584-.732c.062-.116.122-.235.178-.355A1.25 1.25 0 1 0 10.35 6.2c.034-.295.052-.595.052-.9Z"
+				fill="currentTheme"
+				d="M9.353 3C5.849 4.408 3 7.463 3 11.47A9.53 9.53 0 0 0 12.53 21c4.007 0 7.062-2.849 8.47-6.353C8.17 17.065 8.14 8.14 9.353 3z"
 				clip-rule="evenodd"
 			/></svg
-		></button
-	>
-	{#if dropdownOpened}
-		<Menu>
-			{#each optionList as option}
-				<Item>
-					<button
-						aria-label="set theme to {option}"
-						class="inline-flex w-[100%] items-center justify-between rounded-[0.3rem] bg-background/30 px-3 text-left text-[1.1rem] capitalize tracking-wide transition-[background] duration-[0.25s] ease-in-out hover:bg-background_light"
-						on:click={() => {
-							localStorage.theme = document.documentElement.className = option;
-						}}
-					>
-						{option}
-					</button>
-				</Item>
-			{/each}
-		</Menu>
-	{/if}
-</div>
+		>
+	</div>
+</button>
